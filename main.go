14fd6f564e90cb8176b84e9f437223918053a620
bintangandk <124556644+bintangandk@users.noreply.github.com>
2025-08-07 11:58:58 +0700

    learn live reload

--- main.go
+++ main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	//inisialiasai Gin
+	router := gin.Default()
+
+	//membuat route dengan method GET
+	router.GET("/", func(ctx *gin.Context) {
+
+		//return response JSON
+		ctx.JSON(200, gin.H{
+			"message": "Hello Star",
+		})
+	})
+
+	//mulai server dengan port 3000
+	router.Run(":3000")
+}
