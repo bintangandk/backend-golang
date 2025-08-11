4301d374754dddd44238269faf093d7ac024ce9b
bintangandk <124556644+bintangandk@users.noreply.github.com>
2025-08-11 11:40:29 +0700

    Add API (get by id, update,delete, insert) and CORS

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -3,12 +3,14 @@ package controllers
 import (
 	"net/http"
 	"santrikoding/backend-api/database"
+	"santrikoding/backend-api/helpers"
 	"santrikoding/backend-api/models"
 	"santrikoding/backend-api/structs"
 
 	"github.com/gin-gonic/gin"
 )
 
+// Function to find all users
 func FindUsers(c *gin.Context) {
 
 	// Inisialisasi slice untuk menampung data user
@@ -24,4 +26,168 @@ func FindUsers(c *gin.Context) {
 		Data:    Users,
 	})
 
+}
+
+// Function to create a new user
+func CreateUser(c *gin.Context) {
+
+	// Inisialisasi struct untuk request body
+	var req = structs.UserCreateRequest{}
+
+	// Bind JSON request body to the struct
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, structs.ErrorResponse{
+			Success: false,
+			Message: "Validation error",
+			Errors:  helpers.TranslateErrorMessage(err),
+		})
+		return
+	}
+
+	// Check if the username already exists
+	user := models.User{
+		Name:     req.Name,
+		Username: req.Username,
+		Email:    req.Email,
+		Password: helpers.HashPassword(req.Password),
+	}
+
+	if err := database.DB.Create(&user).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, structs.ErrorResponse{
+			Success: false,
+			Message: "Failed to create user",
+			Errors:  helpers.TranslateErrorMessage(err),
+		})
+		return
+	}
+
+	c.JSON(http.StatusCreated, structs.SuccessResponse{
+		Success: true,
+		Message: "User created successfully",
+		Data: structs.UserResponse{
+			Id:        user.Id,
+			Name:      user.Name,
+			Username:  user.Username,
+			Email:     user.Email,
+			CreatedAt: user.CreatedAt.Format("2006-01-02 15:04:05"),
+			UpdatedAt: user.UpdatedAt.Format("2006-01-02 15:04:05"),
+		},
+	})
+
+}
+
+func FindUsersById(c *gin.Context) {
+	id := c.Param("id")
+
+	var user models.User
+
+	if err := database.DB.First(&user, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, structs.ErrorResponse{
+			Success: false,
+			Message: "User not found",
+			Errors:  helpers.TranslateErrorMessage(err),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, structs.SuccessResponse{
+		Success: true,
+		Message: "User found successfully",
+		Data: structs.UserResponse{
+			Id:        user.Id,
+			Name:      user.Name,
+			Username:  user.Username,
+			Email:     user.Email,
+			CreatedAt: user.CreatedAt.Format("2006-01-02 15:04:05"),
+			UpdatedAt: user.UpdatedAt.Format("2006-01-02 15:04:05"),
+		},
+	})
+
+}
+
+func UpdateUser(c *gin.Context) {
+
+	id := c.Param("id")
+
+	var user models.User
+
+	// Find user by ID
+	if err := database.DB.First(&user, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, structs.ErrorResponse{
+			Success: false,
+			Message: "User not found",
+			Errors:  helpers.TranslateErrorMessage(err),
+		})
+		return
+	}
+
+	// struct for request body
+	var req structs.UserUpdateRequest
+
+	// Bind JSON request body to the struct
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusUnprocessableEntity, structs.ErrorResponse{
+			Success: false,
+			Message: "Validation error",
+			Errors:  helpers.TranslateErrorMessage(err),
+		})
+		return
+	}
+
+	user.Name = req.Name
+	user.Username = req.Username
+	user.Email = req.Email
+	user.Password = helpers.HashPassword(req.Password)
+
+	if err := database.DB.Save(&user).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, structs.ErrorResponse{
+			Success: false,
+			Message: "Failed to update user",
+			Errors:  helpers.TranslateErrorMessage(err),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, structs.SuccessResponse{
+		Success: true,
+		Message: "User updated successfully",
+		Data: structs.UserResponse{
+			Id:        user.Id,
+			Name:      user.Name,
+			Username:  user.Username,
+			Email:     user.Email,
+			CreatedAt: user.CreatedAt.Format("2006-01-02 15:04:05"),
+			UpdatedAt: user.UpdatedAt.Format("2006-01-02 15:04:05"),
+		},
+	})
+
+}
+
+func DeleteUser(c *gin.Context) {
+	id := c.Param("id")
+
+	var user models.User
+
+	if err := database.DB.First(&user, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, structs.ErrorResponse{
+			Success: false,
+			Message: "User not found",
+			Errors:  helpers.TranslateErrorMessage(err),
+		})
+		return
+	}
+
+	if err := database.DB.Delete(&user).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, structs.ErrorResponse{
+			Success: false,
+			Message: "Failed to delete user",
+			Errors:  helpers.TranslateErrorMessage(err),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, structs.SuccessResponse{
+		Success: true,
+		Message: "User deleted successfully",
+	})
 }
--- routes/routes.go
+++ routes/routes.go
@@ -4,19 +4,33 @@ import (
 	"santrikoding/backend-api/controllers"
 	"santrikoding/backend-api/middlewares"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter() *gin.Engine {
 	router := gin.Default()
 
-	// Setup routes for user registration
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:  []string{"*"},
+		AllowMethods:  []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:  []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders: []string{"Content-Length"},
+	}))
+
 	router.POST("/api/register", controllers.Register)
 
-	// Setup routes for user login
 	router.POST("/api/login", controllers.Login)
 
 	router.GET("/api/users", middlewares.AuthMiddleware(), controllers.FindUsers)
+
+	router.POST("/api/users", middlewares.AuthMiddleware(), controllers.CreateUser)
+
+	router.GET("/api/users/:id", middlewares.AuthMiddleware(), controllers.FindUsersById)
+
+	router.PUT("/api/users/:id", middlewares.AuthMiddleware(), controllers.UpdateUser)
+
+	router.DELETE("/api/users/:id", middlewares.AuthMiddleware(), controllers.DeleteUser)
 
 	return router
 
